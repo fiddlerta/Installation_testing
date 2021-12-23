36bd48ec8fa3753ec098f4a476b8c9c2a4e8ac37
FiddlerTA <92870947+fiddlerta@users.noreply.github.com>
2021-12-23 10:17:39 +0200

    Updates

--- installation_test/installation.py
+++ installation_test/installation.py
@@ -2,17 +2,14 @@ import os
 import sys
 from datetime import datetime
 
-initial_count = 0
-#dir = str(sys.argv[0])
-#dir = "C://Users//vpfai//Desktop//jmeter_DATA"
-dir = input()
-[x[0] for x in os.walk(dir)]
-dir_tuple = [x[0] for x in os.walk(dir)]
-#print(dir_tuple,sep='\n',end='\n')
-for item in dir_tuple:
+initial_count = 0 #counter
+dir = input() #input directory name to check
+dir_tuple = [x[0] for x in os.walk(dir)] #write all folders & subfolders to the tuple
+
+for item in dir_tuple: 
     print(str(item))
 
-for directory in dir_tuple:
+for directory in dir_tuple: #check all files in sub/folders and count them
     for path in os.listdir(directory):
         if os.path.isfile(os.path.join(directory, path)):
             initial_count += 1
@@ -24,8 +21,7 @@ print("---------")
 
 now = datetime.now() # current date and time
 date_time = now.strftime("%m/%d/%Y, %H:%M:%S")
-#print("Date",date_time)
 
-log_output = ['------', date_time, str(dir),result, '------']
+log_output = ['------', date_time, str(dir),result, '------'] #Write data to log.txt 
 with open('log.txt', 'a') as f:
     f.writelines('\n'.join(log_output))
